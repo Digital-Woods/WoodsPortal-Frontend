075d3a054392d87dae5ab7bf8d61b90477aa0f79
Krish3uuu <krishnamandal400@gmail.com>
2024-11-29 18:05:42 +0530

    add button removed from home tab

--- js/components/UI/DashboardTable.js
+++ js/components/UI/DashboardTable.js
@@ -264,11 +264,13 @@ const DashboardTable = ({ hubspotObjectTypeId, path, inputValue, title, apis, de
 
         {/* {tableData.length > 0 && <Select buttonText="Order: Ascending" />} */}
         {/* {tableAPiData && tableAPiData.data && console.log('tableAPiData', tableAPiData.data.configurations.createFormButton)} */}
-        {(tableAPiData && tableAPiData.data && tableAPiData.data.configurations && tableAPiData.data.configurations.createFormButton) &&
-          <Button variant='create' onClick={() => setShowAddDialog(true)}>
-            <span className="mr-2"> + </span> Create {title}
-          </Button>
-        }
+        {hubSpotUserDetails.sideMenu[0].tabName === title
+          ? null
+          : (tableAPiData && tableAPiData.data && tableAPiData.data.configurations && tableAPiData.data.configurations.createFormButton) && (
+            <Button variant="create" onClick={() => setShowAddDialog(true)}>
+              <span className="mr-2"> + </span> Create {title}
+            </Button>
+          )}
       </div>
 
       {tableData.length > 0 && (
