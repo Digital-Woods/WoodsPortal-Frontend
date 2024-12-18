a54377ed197ac680128ffda2c9240c447129ab92
Krish3uuu <krishnamandal400@gmail.com>
2024-12-18 16:48:04 +0530

    no records found icon and message fix

--- js/components/UI/EmptyMessageCard.js
+++ js/components/UI/EmptyMessageCard.js
@@ -1,20 +1,32 @@
-const EmptyMessageCard = ({name='item'}) => {
+const EmptyMessageCard = ({ name = 'item', type = 'row', className = 'p-6' }) => {
+    const [RandomComponent, setRandomComponent] = useState(null);
+
+    useEffect(() => {
+        const components = [
+            <EmptyDeal width="150" height="130"  key="deal" />,
+            <EmptyIcon width="150" height="130" key="icon" className="dark:text-white text-primary" />,
+            <EmptyThree width="150" height="130" />
+        ];
+        setRandomComponent(components[Math.floor(Math.random() * components.length)]);
+    }, []);
+
     return (
-        <div className="flex flex-col items-center justify-center h-auto text-center space-y-4 dark:bg-dark-300 bg-white p-6 rounded-md">
+        <div className={`mt-4 w-fit max-w-[600px] mx-auto min-h-48 flex ${type === 'row' ? 'max-sm:flex-col' : 'flex-col'} items-center justify-center h-auto text-center md:gap-8 gap-4 dark:bg-dark-300 rounded-md ${className} `}>
             {/* Illustration */}
-            <div className="md:w-20 w-16 md:h-20 h-16 bg-gray-200 dark:bg-dark-200 rounded-full flex items-center justify-center">
-                <EmptyIcon className="md:w-12 w-8 md:h-12 h-8 dark:text-white text-primary" />
+            <div className="md:min-w-[150px] flex items-center justify-center">
+                {RandomComponent}
             </div>
+            <div className="flex flex-col gap-2">
+                {/* Title */}
+                <h2 className={`md:text-xl ${type === 'row' ? 'text-start max-sm:text-center' : 'text-center'} text-lg font-semibold dark:text-white text-primary`}>
+                    No {name.toLowerCase()} created yet
+                </h2>
 
-            {/* Title */}
-            <h2 className="md:text-xl text-lg font-semibold dark:text-white text-primary">
-                No {name.toLowerCase()} available
-            </h2>
-
-            {/* Message */}
-            <p className="md:text-sm text-xs text-primary dark:text-white">
-                It looks like you haven’t created any {name.toLowerCase()} yet.
-            </p>
+                {/* Message */}
+                <p className={`md:text-xs ${type === 'row' ? 'text-start max-sm:text-center' : 'text-center'} text-xs text-primary dark:text-white font-thin`}>
+                    It looks like you haven’t created any {name.toLowerCase()} yet. Please create one to proceed. <br /> If you need assistance with setting up or understanding {name.toLowerCase()}, refer to our documentation or contact support.
+                </p>
+            </div>
         </div>
     );
 };
