172edf016951d4f1cbf7dd55341b8483255f0dbc
Prosenjit Sarkar <bestprosenjit@gmail.com>
2025-02-26 13:49:24 +0530

    fix create issue

--- src/js/utils/param.js
+++ src/js/utils/param.js
@@ -159,8 +159,20 @@ const addParam = (url, params = {}) => {
 //   return mergedObj;
 // }
 
+function getQueryParamsToObject(url) {
+  const params = new URLSearchParams(url);
+  let obj = {};
+  
+  params.forEach((value, key) => {
+      obj[key] = value;
+  });
+
+  return obj;
+}
+
 
 function updateParamsFromUrl(apiUrl, params) {
+  console.log('params', params)
     const url = new URL(apiUrl, "http://example.com"); // Base URL to parse query params
     const searchParams = new URLSearchParams(url.search);
 
