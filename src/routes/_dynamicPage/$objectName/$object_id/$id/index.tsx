b240f8bdb2d93edc9310ee21e23e593a2958b81f
Krish3uuu <krishnamandal400@gmail.com>
2025-08-23 17:34:12 +0530

    Objects iframe properties issue fixed

--- src/routes/_dynamicPage/$objectName/$object_id/$id/index.tsx
+++ src/routes/_dynamicPage/$objectName/$object_id/$id/index.tsx
@@ -24,7 +24,7 @@ const Details = () => {
     : breadcrumbs;
 
   let tabName = breadcrumbItems.length > 0 ? (breadcrumbItems.length > 1 ? breadcrumbItems[breadcrumbItems.length - 2]?.name : breadcrumbItems[0]?.name) : "";
-  
+
   // Find the object in moduleIframeListOptions that matches the objectId
 
   // const matchedObject = 
@@ -33,29 +33,49 @@ const Details = () => {
   // const matchedObject = moduleIframeListOptions.find(
   //   (item) => item.hubspotObjectTypeId === objectId && item.label === tabName
   // ) || '';
-  
+
   // // Extract propertyName and showIframe from the matched object
   // const propertyName = matchedObject.propertyName || '';
   // const showIframe = matchedObject.showIframe || false;
-
-  const matchedObject: any = moduleIframeListOptions.find((item: any) => item.hubspotObjectTypeId === objectId && item.label === tabName) || {};
+  // console.log(objectId, 'objectId');
+  // console.log(tabName, 'tabName');
+  // const matchedObject: any = moduleIframeListOptions.find((item: any) => item.hubspotObjectTypeId === objectId && item.label === tabName) || {};
   // const matchedObject = moduleIframeListOptions.find((item) => item.hubspotObjectTypeId === objectId && item.label === tabName) || {};
-  // console.log(moduleIframeListOptions,'moduleIframeListOptions');
+  // normalize tabName from breadcrumbs
+  const normalizedTabName = (tabName || "")
+    .toLowerCase()
+    .replace(/\s+/g, "-"); // replace spaces with dash like your example
+
+  const matchedObject: any =
+    moduleIframeListOptions.find((item: any) => {
+      return (
+        item.hubspotObjectTypeId === objectId &&
+        (
+          (item.label || "").toLowerCase().replace(/\s+/g, "-") === normalizedTabName ||
+          (item.tabName || "").toLowerCase().replace(/\s+/g, "-") === normalizedTabName
+        )
+      );
+    }) || {};
+
+
+  // console.log(moduleIframeListOptions, 'moduleIframeListOptions');
   // Extract propertyName and showIframe from the matched object
   // const propertyName = matchedObject.properties_value ? matchedObject.properties_value.split(',') : [];
   const propertyName = matchedObject.iframeProperties ? matchedObject.iframeProperties : [];
   const showIframe = matchedObject.showIframe || false;
-
+  // console.log(matchedObject, 'matchedObject');
+  // console.log(propertyName, 'propertyName');
+  // console.log(showIframe, 'showIframe');
   return (
     <div className="bg-[var(--sidebar-background-color)] mt-[calc(var(--nav-height)-1px)] dark:bg-dark-300">
-    <div className={`bg-cleanWhite dark:bg-dark-200`}>
-      {env.VITE_DATA_SOURCE_SET !== true ? (
-        <ApiDetails objectId={objectId} path={path} id={id} propertyName={propertyName} showIframe={showIframe} />
-      ) : (
-        <ModuleDetails objectId={objectId} path={path} id={id} propertyName={propertyName} showIframe={showIframe}  />
-      )
-      }
-    </div>
+      <div className={`bg-cleanWhite dark:bg-dark-200`}>
+        {env.VITE_DATA_SOURCE_SET !== true ? (
+          <ApiDetails objectId={objectId} path={path} id={id} propertyName={propertyName} showIframe={showIframe} />
+        ) : (
+          <ModuleDetails objectId={objectId} path={path} id={id} propertyName={propertyName} showIframe={showIframe} />
+        )
+        }
+      </div>
     </div>
   );
 };
