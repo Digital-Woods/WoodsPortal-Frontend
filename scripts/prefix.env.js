483601a5222b4610de41dd91a80412c84a11026c
Prosenjit Sarkar <bestprosenjit@gmail.com>
2025-09-05 13:40:21 +0530

    fix css env

--- scripts/prefix.env.js
+++ scripts/prefix.env.js
@@ -1,7 +1,7 @@
-const forcePrefix = false
+const forcePrefix = null // true, false, or null to auto-detect
 
 const isPrefix = (env) => {
-  if (forcePrefix) return true
+  if (forcePrefix != null) return forcePrefix
   return env === 'production'
 }
 export default isPrefix
