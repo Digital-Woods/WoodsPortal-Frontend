601903ae93de5bc09b3c2c638c04119b367cf83e
Prosenjit Sarkar <bestprosenjit@gmail.com>
2025-09-24 17:34:51 +0530

    fix ticker api url

--- src/components/ui/Tickets.tsx
+++ src/components/ui/Tickets.tsx
@@ -3,6 +3,7 @@ import { getParam } from '@/utils/param';
 import { getPortal } from '@/data/client/auth-utils';
 import { hubId } from '@/data/hubSpotData'
 import { DynamicComponentView } from '@/components/ui/Table/DynamicComponentView'
+import { getParamDetails } from "@/utils/GenerateUrl";
 
 export const Tickets = ({
   path,
@@ -35,14 +36,17 @@ export const Tickets = ({
 
   const defaultObjectIds = JSON.parse(env.VITE_HUBSPOT_DEFAULT_OBJECT_IDS);
 
+  const {params} = getParamDetails()
+
   const apis = {
-    tableAPI: `/api/${hubId}/${portalId}/hubspot-object-data/${
-      defaultObjectIds.tickets
-    }?parentObjectTypeId=${parentObjectTypeId}&parentObjectRecordId=${parentObjectRowId}&mediatorObjectTypeId=${
-      mediatorObjectTypeId ? mediatorObjectTypeId : parentObjectTypeId
-    }&mediatorObjectRecordId=${
-      mediatorObjectRecordId ? mediatorObjectRecordId : parentObjectRowId
-    }&isPrimaryCompany=${companyAsMediator}`,
+     tableAPI: `/api/${hubId}/${portalId}/hubspot-object-data/${defaultObjectIds.tickets}${params}`,
+    // tableAPI: `/api/${hubId}/${portalId}/hubspot-object-data/${
+    //   defaultObjectIds.tickets
+    // }?parentObjectTypeId=${parentObjectTypeId}&parentObjectRecordId=${parentObjectRowId}&mediatorObjectTypeId=${
+    //   mediatorObjectTypeId ? mediatorObjectTypeId : parentObjectTypeId
+    // }&mediatorObjectRecordId=${
+    //   mediatorObjectRecordId ? mediatorObjectRecordId : parentObjectRowId
+    // }&isPrimaryCompany=${companyAsMediator}`,
     // tableAPI: `/api/${hubId}/${portalId}/hubspot-object-data/${hubspotObjectTypeId}/${id}`,
     stagesAPI: `/api/${hubId}/${portalId}/hubspot-object-pipelines/${defaultObjectIds.tickets}/`, // concat pipelineId
     formAPI: `/api/${hubId}/${portalId}/hubspot-object-forms/${defaultObjectIds.tickets}/fields?isPrimaryCompany=${companyAsMediator}&parentObjectTypeId=${parentObjectTypeId}`,
