3cac1a3dd2ac7569f05972db2fcc4a1267067c96
Prosenjit Sarkar <bestprosenjit@gmail.com>
2025-11-27 18:55:39 +0530

    fix date scroll

--- src/components/Details/DetailsViewDateTimeDialog.tsx
+++ src/components/Details/DetailsViewDateTimeDialog.tsx
@@ -71,10 +71,10 @@ export const DetailsViewDateTimeDialog = ({
     <Dialog
       open={editRow}
       onClose={() => setDateTimeDialog(false)}
-      className="p-4"
       ref={panelRef}
+      className="pr-2 py-4"
     >
-      <div className="rounded-md lg:w-[480px] md:w-[410px] w-[calc(100vw-60px)]  flex-col gap-6 flex">
+      <div className="rounded-md lg:w-[480px] md:w-[410px] w-[calc(100vw-60px)] flex-col gap-6 flex  overflow-auto lg:max-h-auto md:max-h-auto max-h-[90vh] pl-4 pr-2">
         <div className="text-start text-xl dark:text-white font-semibold">
           {editRow?.label || editRow?.customLabel}
         </div>
