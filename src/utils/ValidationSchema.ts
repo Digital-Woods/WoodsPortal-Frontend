fd2b66b24ff2a090d7492b4353e14f492fff473e
Prosenjit Sarkar <bestprosenjit@gmail.com>
2025-11-28 16:07:07 +0530

    add details update validation

---
+++ src/utils/ValidationSchema.ts
@@ -0,0 +1,97 @@
+import z from 'zod'
+import { isArray, isObject } from './DataMigration'
+
+export const ValidationSchemaShape = (value: any, key: any = 'key') => {
+  if (isArray(value) && value.length > 0) {
+    let schemaShape: any = {}
+    value.forEach((field: any) => {
+      schemaShape = {...schemaShape, ...generateSchema(field, key)}
+    })
+    console.log('schemaShape', schemaShape)
+    return schemaShape
+  }
+  if (isObject(value)) {
+    return generateSchema(value, key)
+  }
+  return {}
+}
+
+const generateSchema = (value: any, key: any = 'key') => {
+  let schemaShape: any = {}
+  const keyName = value[key] || ''
+  const fieldName = value?.labels?.plural || value?.customLabel || value?.label
+  const isDomain = keyName === 'domain'
+  const isNumber = value?.fieldType === 'number'
+  const isDate = value?.fieldType === 'date'
+
+  if (value?.requiredField && value?.fieldRole === 'OBJECTS') {
+    schemaShape[keyName] = z
+      .any()
+      .refine((val: any) => Array.isArray(val) && val.length > 0, {
+        message: `${fieldName} must be a non-empty list.`,
+      })
+  } else if (
+    (value?.requiredField || value?.primaryProperty) &&
+    !isDomain &&
+    !isNumber &&
+    !isDate
+  ) {
+    schemaShape[keyName] = z.string().nonempty({
+      message: `${fieldName} is required.`,
+    })
+  } else if (isNumber) {
+    if (value?.requiredField) {
+      // REQUIRED number
+      schemaShape[keyName] = z
+        .string()
+        .nonempty({
+          message: `${fieldName} is required`,
+        })
+        .refine(
+          (value: any) => value === null || value === '' || /^\d+$/.test(value),
+          {
+            message: `Invalid ${fieldName}`,
+          },
+        )
+    } else {
+      // OPTIONAL number
+      schemaShape[keyName] = z
+        .string()
+        .nullable()
+        .optional()
+        .refine(
+          (value: any) => value === null || value === '' || /^\d+$/.test(value),
+          {
+            message: `Invalid ${fieldName}`,
+          },
+        )
+    }
+  } else if (isDate) {
+    if (value?.requiredField) {
+      schemaShape[keyName] = z
+        .any()
+        .refine((val) => val !== null && val !== undefined && val !== '', {
+          message: `${fieldName} is required`,
+        })
+    } else {
+      schemaShape[keyName] = z.any().nullable().optional()
+    }
+  } else if (isDomain) {
+    schemaShape[keyName] = z.string().refine(
+      (value) => {
+        const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        return domainRegex.test(value)
+      },
+      {
+        message: 'Invalid domain format',
+      },
+    )
+  } else {
+    if (value?.fieldRole === 'OBJECTS') {
+      schemaShape[keyName] = z.any().nullable()
+    } else {
+      schemaShape[keyName] = z.string().nullable()
+    }
+  }
+  return schemaShape
+}
