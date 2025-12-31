9e5d9c8649b3fa818927e1779a031923f070253c
prosenjitdigitalwoods <153087894+prosenjitdigitalwoods@users.noreply.github.com>
2025-12-31 17:37:20 +0530

    Prosenjit/2025 (#443)
    * fix focus
    
    * fix pipeline filter condition
    
    * fix register page design
    
    * fix register page
    
    * fix iframe url
    
    * fix error
    
    * skip some fieldType in ifream
    
    * fix link click
    
    * skip multi check
    
    * fix home card
    
    * fix home card
    
    * add ExpireSession
    
    * add loader
    
    * add prelogin email
    
    * change activeStatus
    
    * change activeStatus toUpperCase
    
    * remove activeStatus add status
    
    * prelogin check allowed
    
    * add unauthorized page
    
    * fix pipeline filter
    
    * Fix after logout redirection login  path
    
    * fix pipeline filter
    
    * Fix pipeline filter
    
    * fix create note param
    
    * fix note param
    
    * fix note pearam
    
    * fix update note parma
    
    * add image and file mime type
    
    * add file upload  validation
    
    * fix validaiton
    
    * fix free account note pagination
    
    * fix public path
    
    * fix same file upload issue
    
    * show 403 error
    
    * fix dev env prefix
    
    * hide board view on free account
    
    * add script
    
    * remove script
    
    * remove  script
    
    * fix multi select value
    
    * fix multi select value
    
    * fix ree account date time input
    
    * fix date time format
    
    * change refresh token logic
    
    * Fix refresh  token api call
    
    * add await in getRefreshToken
    
    * fix call refresh token call
    
    * protect unauthorized page
    
    * check refresh token
    
    * add await for get token
    
    * remove await from getAccessToken
    
    * add refresh buffer
    
    * fix axios  token
    
    * fix object select
    
    * fix object multi select payload
    
    * fix client session
    
    * fix public path
    
    * skip refresh  token api call form public path and fix client session
    
    * remove portals array
    
    * fix free account after
    
    * set portal id 1
    
    * fix pipeline filter
    
    * fix attachment upload issue
    
    * forgot password set entredEmail
    
    * fix specPipeLine filter
    
    * fix details view date format
    
    * fix button size
    
    * fix default pipeline id filter
    
    * fix pipeline filter
    
    * fix pipeline filter
    
    * change file note and ticket permissions
    
    * fix note multi api call
    
    * fix select stage validation
    
    * fix pipeline filter
    
    * add parentObjectTypeId in hubspot-object-pipelines api
    
    * home ticket pipeline add parentObjectTypeId
    
    * home ticket if is primary  company then parentObjectTypeId add 0-2
    
    * add view note component
    
    * fix skeleton loader
    
    * home ticket pass parentObjectTypeId param
    
    * fix isPrimaryCompany and hubspotObjectTypeId 0-5 then config from ticket
    
    * add button text color
    
    * add list page group and heading color
    
    * fix list page group json
    
    * list header add dynamic text color
    
    * fix list heading text  color
    
    * fix list page details color
    
    * dynamic change button text color
    
    * fix dynamic button text color
    
    * hide details view card data
    
    * add note view border and bg
    
    * button add default border
    
    * hide theme switch option
    
    * change note view button label
    
    * fix dynamic button color
    
    ---------
    
    Co-authored-by: Prosenjit Sarkar <bestprosenjit@gmail.com>

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,9 +7,11 @@ const classesDynamicClassName = {
   normal: "",
 };
 
+const buttonColor = "text-[var(--button-text-color)] "
+
 const variantDynamicClassName: any = {
   default:
-    "bg-primary text-[var(--button-text-color)] dark:border dark:bg-dark-400 dark:hover:bg-dark-400 shadow hover:bg-primary/90",
+    `${buttonColor} bg-primary dark:border dark:bg-dark-400 dark:hover:bg-dark-400 shadow hover:bg-primary/90`,
   create:
    "CUSTOM-create-button-background-color CUSTOM-create-button-text-color",
   destructive:
@@ -17,7 +19,7 @@ const variantDynamicClassName: any = {
   outline:
     "border border-input text-foreground dark:text-white bg-transparent shadow hover:bg-accent hover:text-accent-foreground dark:bg-dark-400 dark:hover:bg-dark-400",
   secondary:
-    "bg-secondary text-[var(--button-text-color)] shadow hover:bg-secondary/80 dark:border dark:bg-dark-400 dark:hover:bg-dark-400",
+    `${buttonColor} bg-secondary shadow hover:bg-secondary/80 dark:border dark:bg-dark-400 dark:hover:bg-dark-400`,
   ghost:
     "hover:bg-accent hover:text-accent-foreground",
   link:
